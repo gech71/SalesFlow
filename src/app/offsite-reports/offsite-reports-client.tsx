80d0724af0e1e14323ba95f1c8369bf25321fbc2
Getaye Temesgen <tgech71@gmail.com>
2025-07-02 12:20:20 +0000

    insert this logo on the sidebar haeder and remove the text 'salesflow' w

--- src/app/offsite-reports/offsite-reports-client.tsx
+++ src/app/offsite-reports/offsite-reports-client.tsx
@@ -78,9 +78,9 @@ export default function OffsiteReportsClient({ user, permissions, leads, distanc
     <SidebarProvider>
       <Sidebar>
         <SidebarHeader>
-            <div className="flex items-center gap-2 p-2">
-                <Icons.workflow className="w-6 h-6 text-primary" />
-                <h2 className="font-semibold text-lg">SalesFlow</h2>
+            <div className="flex items-center justify-center p-2">
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img src="https://play-lh.googleusercontent.com/bXqMt9ROsGd0H9vPhib5hG-0NB-EJcAwZy6UUDhvlP-ykE595IMQtzr14R6IRWtJiGTh=w600-h300-pc0xffffff-pd" alt="NIB International Bank Logo" className="h-12 w-auto" />
             </div>
         </SidebarHeader>
         <SidebarContent>
