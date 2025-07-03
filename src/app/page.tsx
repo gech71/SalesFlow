b1b29f232398ee5aeb98d2791f2f1652c174c5cd
Lykamopia <www.alex94lykam@gmail.com>
2025-07-03 14:23:54 +0000

    Updated app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,6 @@ import {
   CardTitle,
 } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import { Icons } from '@/components/icons';
 import { useToast } from "@/hooks/use-toast";
 import { loginAction } from './actions';
@@ -70,7 +69,7 @@ export default function LoginPage() {
         <CardHeader className="text-center">
             <div className="flex flex-col items-center justify-center gap-2 mb-4">
                 {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img src="https://play-lh.googleusercontent.com/bXqMt9ROsGd0H9vPhib5hG-0NB-EJcAwZy6UUDhvlP-ykE595IMQtzr14R6IRWtJiGTh=w600-h300-pc0xffffff-pd" alt="NIB International Bank Logo" className="h-16 w-auto" />
+                <img src="https://th.bing.com/th/id/R.f76dabe4fac17634185beac29762498b?rik=VcpX%2Bw6udP0tgA&riu=http%3a%2f%2fwww.ethioxchange.com%2fstorage%2fbanks%2flogo%2f01J73Y8N756BVZ9PPKF60ZYFM0.png&ehk=IB1kPIaDd2GDbC2Ur5HlQKTKS37a6%2bglIr8W58E5PzQ%3d&risl=&pid=ImgRaw&r=0" alt="NIB International Bank Logo" className="h-16 w-auto" />
                 <CardTitle className="text-primary">Nib Sales</CardTitle>
             </div>
           <CardDescription>Enter your credentials to access your account.</CardDescription>
@@ -78,22 +77,21 @@ export default function LoginPage() {
         <CardContent>
           <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
             <div className="space-y-2">
-              <Label htmlFor="phoneNumber">Phone Number</Label>
               <Input
                 id="phoneNumber"
                 type="tel"
-                placeholder="0912345678"
+                placeholder="Phone Number"
                 {...register('phoneNumber')}
                 disabled={isSubmitting}
               />
               {errors.phoneNumber && <p className="text-sm text-destructive">{errors.phoneNumber.message}</p>}
             </div>
             <div className="space-y-2">
-              <Label htmlFor="password">Password</Label>
               <div className="relative">
                 <Input 
                   id="password" 
                   type={showPassword ? 'text' : 'password'}
+                  placeholder="Password"
                   {...register('password')} 
                   disabled={isSubmitting}
                   className="pr-10"
@@ -119,5 +117,3 @@ export default function LoginPage() {
     </div>
   );
 }
-
-    
