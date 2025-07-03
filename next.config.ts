307cccdf5db5a35e4b02522f3d6197bb6e80b26f
Lykamopia <www.alex94lykam@gmail.com>
2025-07-03 12:10:37 +0000

    you haven't fixed the issue 'Manifest: Line: 1, column: 1, Syntax error.

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 import withPWAInit from "@ducanh2912/next-pwa";
 
@@ -5,7 +6,6 @@ const withPWA = withPWAInit({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
 });
 
 const nextConfig: NextConfig = {
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,10 +9,6 @@ export const metadata: Metadata = {
   title: 'Sales Lead Management',
   description: 'Sales Lead Management for NIB International Bank',
   manifest: '/manifest.json',
-  icons: {
-    icon: 'https://play-lh.googleusercontent.com/bXqMt9ROsGd0H9vPhib5hG-0NB-EJcAwZy6UUDhvlP-ykE595IMQtzr14R6IRWtJiGTh=w600-h300-pc0xffffff-pd',
-    apple: 'https://play-lh.googleusercontent.com/bXqMt9ROsGd0H9vPhib5hG-0NB-EJcAwZy6UUDhvlP-ykE595IMQtzr14R6IRWtJiGTh=w180-h180-pc0xffffff-pd'
-  },
 };
 
 export const viewport: Viewport = {
