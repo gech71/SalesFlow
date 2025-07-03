9224ac71058454d42a08246fd6195a8ab7c98d0c
Lykamopia <www.alex94lykam@gmail.com>
2025-07-03 14:21:51 +0000

    have you seen this when the side bar is collapsed it looks wierd , so re

--- src/app/offsite-reports/offsite-reports-client.tsx
+++ src/app/offsite-reports/offsite-reports-client.tsx
@@ -43,9 +43,9 @@ export default function OffsiteReportsClient({ user, permissions, reports }: { u
     <SidebarProvider>
       <Sidebar collapsible="icon">
         <SidebarHeader>
-            <Link href="/dashboard" className="flex items-center gap-2 p-2 group-data-[state=collapsed]/sidebar-wrapper:justify-center">
-                <img src="https://th.bing.com/th/id/R.f76dabe4fac17634185beac29762498b?rik=VcpX%2Bw6udP0tgA&riu=http%3a%2f%2fwww.ethioxchange.com%2fstorage%2fbanks%2flogo%2f01J73Y8N756BVZ9PPKF60ZYFM0.png&ehk=IB1kPIaDd2GDbC2Ur5HlQKTKS37a6%2bglIr8W58E5PzQ%3d&risl=&pid=ImgRaw&r=0" alt="NIB Sales Logo" className="h-10 w-auto transition-all group-data-[state=collapsed]/sidebar-wrapper:h-8" />
-                <h2 className="font-semibold text-lg text-primary group-data-[state=collapsed]/sidebar-wrapper:hidden">NIB Sales</h2>
+            <Link href="/dashboard" className="flex items-center gap-2 p-2 group-data-[state=collapsed]:justify-center">
+                <img src="https://th.bing.com/th/id/R.f76dabe4fac17634185beac29762498b?rik=VcpX%2Bw6udP0tgA&riu=http%3a%2f%2fwww.ethioxchange.com%2fstorage%2fbanks%2flogo%2f01J73Y8N756BVZ9PPKF60ZYFM0.png&ehk=IB1kPIaDd2GDbC2Ur5HlQKTKS37a6%2bglIr8W58E5PzQ%3d&risl=&pid=ImgRaw&r=0" alt="NIB Sales Logo" className="h-10 w-auto transition-all group-data-[state=collapsed]:h-7" />
+                <h2 className="font-semibold text-lg text-primary group-data-[state=collapsed]:hidden">NIB Sales</h2>
             </Link>
         </SidebarHeader>
         <SidebarContent>
@@ -196,5 +196,7 @@ export default function OffsiteReportsClient({ user, permissions, reports }: { u
     </SidebarProvider>
   );
 }
+
+    
 
     
