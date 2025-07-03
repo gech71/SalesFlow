65cdd1ec26665e49a14afde73bfff819a9bc821d
Lykamopia <www.alex94lykam@gmail.com>
2025-07-03 13:53:52 +0000

    Make the logo in the sidebar clickable so that when it is clicked, it re

--- src/app/branch-assignments/branch-assignments-client.tsx
+++ src/app/branch-assignments/branch-assignments-client.tsx
@@ -141,10 +141,10 @@ export default function BranchAssignmentsClient({ user, permissions, leads, bran
     <SidebarProvider>
       <Sidebar>
         <SidebarHeader>
-            <div className="flex items-center gap-2 p-2 justify-center">
+            <Link href="/dashboard" className="flex items-center gap-2 p-2">
                 <img src="https://th.bing.com/th/id/R.f76dabe4fac17634185beac29762498b?rik=VcpX%2bw6udP0tgA&riu=http%3a%2f%2fwww.ethioxchange.com%2fstorage%2fbanks%2flogo%2f01J73Y8N756BVZ9PPKF60ZYFM0.png&ehk=IB1kPIaDd2GDbC2Ur5HlQKTKS37a6%2bglIr8W58E5PzQ%3d&risl=&pid=ImgRaw&r=0" alt="NIB Sales Logo" className="h-10 w-auto" />
                 <h2 className="font-semibold text-lg text-primary">NIB Sales</h2>
-            </div>
+            </Link>
         </SidebarHeader>
         <SidebarContent>
             <SidebarMenu>
