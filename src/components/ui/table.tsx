a826fbf8a1b19cb3a668c6364d4df2c7f1e53756
Lykamopia <www.alex94lykam@gmail.com>
2025-07-03 09:32:41 +0000

    Unhandled Runtime Error
    Error: TableCell is not defined
    
    Call Stack
    5

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -81,6 +81,18 @@ const TableHead = React.forwardRef<
 ))
 TableHead.displayName = "TableHead"
 
+const TableCell = React.forwardRef<
+  HTMLTableCellElement,
+  React.TdHTMLAttributes<HTMLTableCellElement>
+>(({ className, ...props }, ref) => (
+  <td
+    ref={ref}
+    className={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0", className)}
+    {...props}
+  />
+))
+TableCell.displayName = "TableCell"
+
 const TableCaption = React.forwardRef<
   HTMLTableCaptionElement,
   React.HTMLAttributes<HTMLTableCaptionElement>
