ab9bbc012bc8759d5508f13de4ca4694b17d386b
Lykamopia <www.alex94lykam@gmail.com>
2025-07-04 08:22:59 +0000

    Manifest: Line: 1, column: 1, Syntax error.Understand this errorEnsure t

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,27 @@ const withPWA = withPWAInit({
   dest: "public",
   register: true,
   skipWaiting: true,
+  manifest: {
+    name: 'Sales Lead Management',
+    short_name: 'NIB Sales',
+    description: 'Sales Lead Management for NIB International Bank',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#F3B000',
+    theme_color: '#F3B000',
+    icons: [
+      {
+        src: 'https://th.bing.com/th/id/R.f76dabe4fac17634185beac29762498b?rik=VcpX%2Bw6udP0tgA&riu=http%3a%2f%2fwww.ethioxchange.com%2fstorage%2fbanks%2flogo%2f01J73Y8N756BVZ9PPKF60ZYFM0.png&ehk=IB1kPIaDd2GDbC2Ur5HlQKTKS37a6%2bglIr8W58E5PzQ%3d&risl=&pid=ImgRaw&r=0',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: 'https://th.bing.com/th/id/R.f76dabe4fac17634185beac29762498b?rik=VcpX%2Bw6udP0tgA&riu=http%3a%2f%2fwww.ethioxchange.com%2fstorage%2fbanks%2flogo%2f01J73Y8N756BVZ9PPKF60ZYFM0.png&ehk=IB1kPIaDd2GDbC2Ur5HlQKTKS37a6%2bglIr8W58E5PzQ%3d&risl=&pid=ImgRaw&r=0',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  },
 });
 
 const nextConfig: NextConfig = {
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,6 @@ import { HoneycombBackground } from '@/components/honeycomb-background';
 export const metadata: Metadata = {
   title: 'Sales Lead Management',
   description: 'Sales Lead Management for NIB International Bank',
-  manifest: '/manifest.json',
   icons: {
     icon: 'https://th.bing.com/th/id/R.f76dabe4fac17634185beac29762498b?rik=VcpX%2Bw6udP0tgA&riu=http%3a%2f%2fwww.ethioxchange.com%2fstorage%2fbanks%2flogo%2f01J73Y8N756BVZ9PPKF60ZYFM0.png&ehk=IB1kPIaDd2GDbC2Ur5HlQKTKS37a6%2bglIr8W58E5PzQ%3d&risl=&pid=ImgRaw&r=0',
   },
@@ -42,5 +41,3 @@ export default function RootLayout({
     </html>
   );
 }
-
-    
