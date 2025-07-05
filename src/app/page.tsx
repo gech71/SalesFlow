080be0352354992f3c6504ea1e3b1ee4fc7ec6c9
Lykamopia <www.alex94lykam@gmail.com>
2025-07-05 07:03:08 +0000

    Add a warm and professional welcome message to the login page, positione

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -72,7 +72,7 @@ export default function LoginPage() {
                 <img src="https://th.bing.com/th/id/R.f76dabe4fac17634185beac29762498b?rik=VcpX%2Bw6udP0tgA&riu=http%3a%2f%2fwww.ethioxchange.com%2fstorage%2fbanks%2flogo%2f01J73Y8N756BVZ9PPKF60ZYFM0.png&ehk=IB1kPIaDd2GDbC2Ur5HlQKTKS37a6%2bglIr8W58E5PzQ%3d&risl=&pid=ImgRaw&r=0" alt="NIB International Bank Logo" className="h-16 w-auto" />
                 <CardTitle className="text-primary">Nib Sales</CardTitle>
             </div>
-          <CardDescription>Enter your credentials to access your account.</CardDescription>
+          <CardDescription>Welcome back! Please log in to continue.</CardDescription>
         </CardHeader>
         <CardContent>
           <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
