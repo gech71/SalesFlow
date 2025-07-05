ddea5cab96c5febdaa81269aa22ce4641fa9fc42
Lykamopia <www.alex94lykam@gmail.com>
2025-07-05 07:02:00 +0000

    Ensure the project meets all requirements to trigger the native browser

--- next.config.ts
+++ next.config.ts
@@ -11,13 +11,13 @@ const withPWA = withPWAInit({
     document: "/offline",
   },
   manifest: {
-    name: 'Sales Lead Management',
-    short_name: 'NIB Sales',
-    description: 'Sales Lead Management for NIB International Bank',
+    name: 'LeadTrack Pro',
+    short_name: 'LeadTrack Pro',
+    description: 'A sales lead tracking and management application.',
     start_url: '/',
     display: 'standalone',
-    background_color: '#F3B000',
-    theme_color: '#F3B000',
+    background_color: '#2E9AFE',
+    theme_color: '#2E9AFE',
     icons: [
       {
         src: 'https://th.bing.com/th/id/R.f76dabe4fac17634185beac29762498b?rik=VcpX%2Bw6udP0tgA&riu=http%3a%2f%2fwww.ethioxchange.com%2fstorage%2fbanks%2flogo%2f01J73Y8N756BVZ9PPKF60ZYFM0.png&ehk=IB1kPIaDd2GDbC2Ur5HlQKTKS37a6%2bglIr8W58E5PzQ%3d&risl=&pid=ImgRaw&r=0',
